67c23a59e6cf1e9bb4c59df9796dea56f2daa905
Joshua Williams <jmw0426@hotmail.com>
2020-03-26 19:50:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,11 +2,11 @@ def reverse_each_word(argument)
    words = argument.split(" ")
    sentence = []
    words.each do |word|
-   end_loop = word.length 
-   new_words = ''
-   while end_loop > 0
-   end_loop -= 1 
-   new_words += word[end_loop]
+     end_loop = word.length 
+     new_words = ''
+     while end_loop > 0
+      end_loop -= 1 
+      new_words += word[end_loop]
    
    
    
